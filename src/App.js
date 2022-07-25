8e472f7184dc6c54889cc4836210c573ac6442ab
Diego Allies <diegoallies27@gmail.com>
2022-07-25 11:43:59 +0200

    video 8 content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
 
 function App() {
+  // const title = "Welcome to Diego's react code";
+  // const likes = "50";
+  // const person = { name: 'Diego', age: '19'};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <div className="content">
+        <Home/>
+        {/* <h1> {title} </h1>
+        <p>liked {likes} times</p>
+        <p> {person.name} {person.age} </p> */}
+      </div>
     </div>
   );
 }
