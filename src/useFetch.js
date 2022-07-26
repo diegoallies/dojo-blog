a98bc875cf9df76799b7fb3f2f7c96de40aafb5c
Diego Allies <diegoallies27@gmail.com>
2022-07-26 11:50:21 +0200

    finished react course

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,10 +6,10 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    // const abortCont = new AbortController();/// { signal: abortCont.signal }
+    const abortCont = new AbortController();
 
     setTimeout(() => {
-      fetch(url,)
+      fetch(url, { signal: abortCont.signal })
       .then(res => {
         if (!res.ok) { // error coming back from server
           throw Error('could not fetch the data for that resource');
@@ -22,18 +22,18 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch(err => {
-        // if (err.name === 'AbortError') {
-        //   console.log('fetch aborted')
-        // } else {}
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted')
+        } else {
           // auto catches network / connection error
           setIsPending(false);
           setError(err.message);
-        
+        }
       })
-    }, 10);
+    }, 1000);
 
     // abort the fetch
-    // return () => abortCont.abort();
+    return () => abortCont.abort();
   }, [url])
 
   return { data, isPending, error };
